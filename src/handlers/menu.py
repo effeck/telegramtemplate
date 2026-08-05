83a38374a905f9047f555db2da9ab6a7e54c5a32
effeck <infiqw@mail.ru>
2026-08-06 01:00:37 +0300

    Update menu.py

--- src/handlers/menu.py
+++ src/handlers/menu.py
@@ -1,16 +1,15 @@
 from typing import Any
-
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
 
-async def menu(update: Update, context: Any)-> None:
+async def menu(update: Update, context: Any) -> None:
     keyboard = [
-        [InlineKeyboardButton("Inter", callback_data="op1")],
-        [InlineKeyboardButton("Empate", callback_data="op2")],
-        [InlineKeyboardButton("Grêmio", callback_data="op3")],
+        [InlineKeyboardButton("📋 Информация", callback_data="info")],
+        [InlineKeyboardButton("📊 Статистика", callback_data="stats")],
+        [InlineKeyboardButton("⚙️ Настройки", callback_data="settings")],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     if update.message:
-        await update.message.reply_text("Escolha uma opção:", reply_markup=reply_markup)
+        await update.message.reply_text("📌 Выберите пункт меню:", reply_markup=reply_markup)
     elif update.callback_query:
-        await update.callback_query.edit_message_text("Escolha uma opção:", reply_markup=reply_markup)
+        await update.callback_query.edit_message_text("📌 Выберите пункт меню:", reply_markup=reply_markup)
